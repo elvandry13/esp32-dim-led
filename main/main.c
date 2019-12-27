150a509b08c7ba24729bfd8756fe44d363e4c20e
Elvandry Ghiffary <elvandry@warungpintar.co>
2019-12-28 00:06:35 +0700

    add pwm function

--- main/main.c
+++ main/main.c
@@ -1,15 +1,18 @@
+#include "adc1.h"
 #include "pwm1.h"
 
 void app_main()
-{    
+{
+    adc_config();
     pwm_config();
-    
-    while (1) {
-        pwm_main(4000);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        pwm_main(2000);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        pwm_main(0);
+    while(1)
+    {
+        int pot_in = adc_main();
+        printf("Potensio input value = %d\n", pot_in);
+        // TO DO -> add map function here
+        int pwm_out;
+        printf("PWM output value = %d\n", pwm_out);
+        pwm_main(pwm_out);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
